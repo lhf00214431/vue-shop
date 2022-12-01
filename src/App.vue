b5b46916d85dc39dc3fcacbd854fdc378df1bcfc
liuhaifeng <517983663@qq.com>
2022-12-01 15:44:45 +0800

    顶部

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe67f;</span>
+      地址地址地支地址地址地址地支地址地址地址地支地址地址地址地支地址
+      <span class="iconfont position__notice">&#xe729;</span>
+    </div>
+  </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
       <div class="iconfont">&#xe7a7;</div>
@@ -20,6 +27,35 @@
 </template>
 
 <style lang="scss">
+@import './style/viriables.scss';
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  bottom: 0.5rem;
+  top: 0;
+  right: 0;
+  padding: 0 .18rem;
+  .position {
+    color: $content-font-color;
+    position: relative;
+    padding:  .16rem .24rem .16rem 0;
+    line-height: .22rem;
+    font-size: .16rem;
+    @include ellipsis;
+    &__icon {
+      position: relative;
+      top: .02rem;
+      font-size: .2rem;
+    }
+    &__notice {
+      position: absolute;
+      top: .18rem;
+      right: 0;
+      font-size: .2rem;
+    }
+  }
+}
 .docker {
   display: flex;
   box-sizing: border-box;
@@ -29,7 +65,8 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top: 1px solid #F1F1F1;
+  border-top: 0.01rem solid #F1F1F1;
+  color: $content-font-color;
   &__item {
     flex: 1;
     text-align: center;
@@ -42,7 +79,7 @@
     }
   }
   &__title {
-    font-size: 20px;
+    font-size: .2rem;
     transform: scale(0.5 0.5);
     transform-origin: center top;
   }
